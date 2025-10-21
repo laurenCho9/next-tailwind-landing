8c03bd33c6309d90c81eb33804ddd41b6e54b32c
laurenCho9 <117485593+laurenCho9@users.noreply.github.com>
2025-10-21 20:19:01 +0900

    feat: 우측 사이드 아이콘 구현(map돌려서 구현했다가 아이콘 사이즈가 제각각이라 absolute 때문에 롤백)

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,42 +1,14 @@
-import Image from "next/image";
 import HeaderLogo from "./HeaderLogo";
 import HeaderMenu from "./HeaderMenu";
+import HeaderSideIcons from "./HeaderSideIcons";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-center py-6 border-b border-gray-200 bg-[#FAF8F6]">
-      <div className="max-w-[1440px] bg-pink-100 flex items-center justify-between px-15 w-full">
+    <header className="flex items-center justify-center py-6 bg-[#FAF8F6]">
+      <div className="max-w-[1440px] flex items-center justify-between px-15 w-full">
         <HeaderLogo />
         <HeaderMenu />
-        <div className="flex items-center justify-end gap-4">
-          <button className="flex items-center justify-center w-8 h-8 bg-white border border-gray-300 rounded-full shadow-md hover:bg-gray-100">
-            <Image
-              src="/icons/alram.svg"
-              alt="alram"
-              width={16}
-              height={18}
-              priority={true}
-            />
-          </button>
-          <button className="flex items-center justify-center w-8 h-8 bg-white border border-gray-300 rounded-full shadow-md hover:bg-gray-100">
-            <Image
-              src="/icons/alram.svg"
-              alt="alram"
-              width={16}
-              height={18}
-              priority={true}
-            />
-          </button>
-          <button className="flex items-center justify-center w-8 h-8 bg-white border border-gray-300 rounded-full shadow-md hover:bg-gray-100">
-            <Image
-              src="/icons/alram.svg"
-              alt="alram"
-              width={16}
-              height={18}
-              priority={true}
-            />
-          </button>
-        </div>
+        <HeaderSideIcons />
       </div>
     </header>
   );
