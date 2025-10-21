1ccd2f22d56fede7e8d4ee6453ddc1e465e14e67
laurenCho9 <117485593+laurenCho9@users.noreply.github.com>
2025-10-21 21:08:26 +0900

    feat: 헤더 fixed 고정

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,7 +4,7 @@ import HeaderSideIcons from "./HeaderSideIcons";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-center py-6 bg-[#FAF8F6]">
+    <header className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center py-6 bg-[#FAF8F6]">
       <div className="max-w-[1440px] flex items-center justify-between px-15 w-full">
         <HeaderLogo />
         <HeaderMenu />
